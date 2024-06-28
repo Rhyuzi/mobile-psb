5df62ddedea5b81ec0f57fe6fe5adc965b5f8609
rhyuzi <azitom2@gmail.com>
2024-06-28 16:27:57 +0700

    add pagination in history

--- src/views/POD.vue
+++ src/views/POD.vue
@@ -12,7 +12,8 @@
                     slot="end"></ion-icon>
                 <ion-icon @click="onClickSearch" class="ic-toolbar" :icon="inSearch.icon" slot="end"></ion-icon>
             </ion-toolbar>
-            <ion-searchbar class="bg-menu-bar" v-if="isSearch" placeholder="Search Data ..." v-model="searchData"></ion-searchbar>
+            <ion-searchbar class="bg-menu-bar" v-if="isSearch" placeholder="Search Data ..."
+                v-model="searchData"></ion-searchbar>
             <ion-segment value="default" class="segment-main">
                 <ion-segment-button @click="state.selectedSegment = 'request'" value="request" class="color-white"
                     :class="{ 'segment-button-checked': state.selectedSegment == 'request' }">
@@ -25,8 +26,8 @@
             </ion-segment>
         </ion-header>
         <ion-content :fullscreen="true">
-            <div v-if="state.selectedSegment == 'history'">
-                <div class="item-pick" v-for="pickup in onSearchDataHistory" :key="pickup.POrderNo">
+            <div v-if="state.selectedSegment == 'history'" class="main-history">
+                <div class="item-pick" v-for="pickup in paginatedHistoryData" :key="pickup.POrderNo">
                     <ion-card-content @click="seeDetail(pickup.POrderID)">
                         <ion-list>
                             <div class="display-flex">
@@ -52,7 +53,7 @@
                 </div>
             </div>
 
-            <div v-if="state.selectedSegment == 'request'">
+            <div v-if="state.selectedSegment == 'request'" class="main-request">
                 <div class="item-pick" v-for="pickup in onSearchData" :key="pickup.POrderNo">
                     <ion-card-content @click="seeDetail(pickup.POrderID)">
                         <ion-list>
@@ -78,10 +79,14 @@
                     </ion-card-content>
                 </div>
             </div>
+            <div v-if="state.selectedSegment == 'history'" class="pagination-controls">
+                <ion-button @click="prevPage" :disabled="currentPage === 1">Previous</ion-button>
+                <span>Page {{ currentPage }} of {{ totalPages }}</span>
+                <ion-button @click="nextPage" :disabled="currentPage === totalPages">Next</ion-button>
+            </div>
         </ion-content>
     </ion-page>
 </template>
-
 
 <script setup lang="ts">
 import {
@@ -102,6 +107,7 @@ import {
     IonPopover,
     IonNote,
     IonModal,
+    IonButton,
     IonBackButton,
     IonSegment,
     IonSegmentButton,
@@ -126,8 +132,20 @@ const state = reactive({
 const searchData = ref("")
 const dataUser = JSON.parse(localStorage.user)
 const modal = ref();
-// const selectedSegment = ref('request');
+const currentPage = ref(1); // Current page number
+const itemsPerPage = 20;
 
+const prevPage = () => {
+    if (currentPage.value > 1) {
+        currentPage.value--;
+    }
+};
+
+const nextPage = () => {
+    if (currentPage.value < totalPages.value) {
+        currentPage.value++;
+    }
+};
 onMounted(async () => {
     if (pickups.value.length == 0) {
         getPickupOrder()
@@ -143,6 +161,7 @@ onMounted(async () => {
             state.selectedSegment = 'request'
         }
     }
+    console.error('totalPages', totalPages.value)
 })
 
 const pickups = computed(() => {
@@ -164,19 +183,18 @@ const onSearchData = computed(() => {
     )
 })
 
-const onSearchDataHistory = computed(() => {
+const filteredHistoryData = computed(() => {
     const pickupArray = Object.values(store.getters['pickup/get']('historyList') as IPickupItem || {})
     return pickupArray.filter(data =>
         data.POrderCustName.toLocaleLowerCase().includes(searchData.value.toLowerCase())
     )
 })
 
-const cancel = () => modal.value.$el.dismiss();
-
-// const openHistory = () => {
-//     modal.value.$el.present()
-//     console.debug('History', modal.value.$el)
-// }
+const paginatedHistoryData = computed(() => {
+    const startIndex = (currentPage.value - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return filteredHistoryData.value.slice(startIndex, endIndex);
+});
 
 const onClickRefresh = () => {
     const refreshIcon = document.querySelector('.refresh-icon');
@@ -247,14 +265,32 @@ const seeDetail = (idPickup: any) => {
     console.debug('idPickup', idPickup)
     router.push("/select-room");
 }
-// const seeDetailHistory = (idPickup: any) => {
-//     localStorage.setItem("idPickup", idPickup)
-//     modal.value.$el.dismiss()
-//     router.push("/select-room");
-// }
+
+const totalPages = computed(() => {
+    const totalItems = Object.values(store.getters['pickup/get']('historyList') as IPickupItem[] || []).length;
+    return Math.ceil(totalItems / itemsPerPage);
+});
+
 onIonViewWillEnter(() => {
     // console.warn('datassssss',onSearchData)
     // console.warn('datassssss 22',pickups)
 });
 
 </script>
+<style scoped>
+.pagination-controls {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    background-color: white;
+    padding: 10px;
+    box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.1);
+    z-index: 1000;
+}
+.main-history {
+    margin-bottom: 100px;
+}
+</style>
