469f20e25276e0a9c1a6c8dfcb6c5d9345e6fa22
rhyuzi <azitom2@gmail.com>
2024-07-02 10:43:53 +0700

    add skeleton ui

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -18,35 +18,71 @@
             </ion-toolbar>
         </ion-header>
         <ion-content :fullscreen="true">
+            <ion-refresher slot="fixed" @ionRefresh="handleRefresh($event)">
+                <ion-refresher-content></ion-refresher-content>
+            </ion-refresher>
             <ion-content class="ion-padding">
                 <div class="display-fl align-center">
                     <h5>{{ dataUser.name }}</h5>
                     <img src="../assets/image/psb-logo-head.png" class="margin-left-auto img-user-icon" alt="">
                 </div>
-                <div class="content-pickup">
-                    <div class="display-fl align-center">
-                        <p>PENGANTARAN (Delivery)</p>
-                        <p class="margin-left-auto label-kg">({{
-                            calculateTotalDeliveryWeight(countDeliveryReq) }} kg)</p>
-                    </div>
-                    <div class="display-fl align-center">
-                        <button @click="toDelReq" class="btn-pickup">{{ countDeliveryReq.length }}</button>
-                        <button @click="toDelHist" class="btn-pickup margin-left-auto"> {{ countDeliveryHist.length }}</button>
-                    </div>
+
+                <div v-if="!loaded">
+                    <ion-list>
+                        <ion-list-header>
+                            <ion-skeleton-text :animated="true" style="width: 100%;height: 22px"></ion-skeleton-text>
+                        </ion-list-header>
+                        <ion-item>
+                            <ion-thumbnail slot="start" style="width: 44%;height: 84px;">
+                                <ion-skeleton-text :animated="true"></ion-skeleton-text>
+                            </ion-thumbnail>
+                            <ion-thumbnail slot="end" style="width: 88%;height: 84px;">
+                                <ion-skeleton-text :animated="true"></ion-skeleton-text>
+                            </ion-thumbnail>
+                        </ion-item>
+                    </ion-list>
+                    <ion-list>
+                        <ion-list-header>
+                            <ion-skeleton-text :animated="true" style="width: 100%;height: 22px"></ion-skeleton-text>
+                        </ion-list-header>
+                        <ion-item>
+                            <ion-thumbnail slot="start" style="width: 44%;height: 84px;">
+                                <ion-skeleton-text :animated="true"></ion-skeleton-text>
+                            </ion-thumbnail>
+                            <ion-thumbnail slot="end" style="width: 88%;height: 84px;">
+                                <ion-skeleton-text :animated="true"></ion-skeleton-text>
+                            </ion-thumbnail>
+                        </ion-item>
+                    </ion-list>
                 </div>
 
-                <div class="content-pickup">
-                    <div class="display-fl align-center">
-                        <p>PENJEMPUTAN (Pick-Up)</p>
-                        <p class="margin-left-auto label-kg">({{
-                            calculateTotalPOrderWeight(pickups) }} kg)</p>
+                <div v-if="loaded">
+                    <div class="content-pickup">
+                        <div class="display-fl align-center">
+                            <p>PENGANTARAN (Delivery)</p>
+                            <p class="margin-left-auto label-kg">({{
+                                calculateTotalDeliveryWeight(countDeliveryReq) }} kg)</p>
+                        </div>
+                        <div class="display-fl align-center">
+                            <button @click="toDelReq" class="btn-pickup">{{ countDeliveryReq.length }}</button>
+                            <button @click="toDelHist" class="btn-pickup margin-left-auto"> {{ countDeliveryHist.length
+                                }}</button>
+                        </div>
                     </div>
-                    <div class="display-fl align-center">
-                        <button @click="toPodReq" class="btn-pickup"
-                            :class="{ 'background-blue color-white': pickups.length !== 0 }">{{ pickups.length
-                            }}</button>
-                        <button @click="toPodHist" class="btn-pickup margin-left-auto">{{ pickupsHistory.length
-                            }}</button>
+
+                    <div class="content-pickup">
+                        <div class="display-fl align-center">
+                            <p>PENJEMPUTAN (Pick-Up)</p>
+                            <p class="margin-left-auto label-kg">({{
+                                calculateTotalPOrderWeight(pickups) }} kg)</p>
+                        </div>
+                        <div class="display-fl align-center">
+                            <button @click="toPodReq" class="btn-pickup"
+                                :class="{ 'background-blue color-white': pickups.length !== 0 }">{{ pickups.length
+                                }}</button>
+                            <button @click="toPodHist" class="btn-pickup margin-left-auto">{{ pickupsHistory.length
+                                }}</button>
+                        </div>
                     </div>
                 </div>
             </ion-content>
@@ -71,9 +107,8 @@ import {
     IonItem,
     IonLabel,
     onIonViewWillEnter,
-    IonPopover,
-    IonNote,
-    IonModal
+    IonRefresher,
+    IonRefresherContent
 } from '@ionic/vue'
 import { personCircle, searchCircle, wifi, search, close, refreshOutline, menuOutline, listCircle, arrowBackOutline } from 'ionicons/icons'
 import { useStore } from 'vuex'
@@ -88,16 +123,28 @@ const ionRouter = useIonRouter()
 // const pickups = ref<IPickupItem[]>([])
 // const pickupsHistory = ref<IPickupItem[]>([])
 const searchData = ref("")
+const loaded = ref(false);
 const dataUser = JSON.parse(localStorage.user)
 const modal = ref();
 
 onMounted(async () => {
-    getPickupHistory()
-    getPickupOrder()
-    getDelivery()
-    getDeliveryHistory()
+    await getPickupHistory()
+    await getPickupOrder()
+    await getDelivery()
+    await getDeliveryHistory()
+    loaded.value = true
     console.error("datas", pickups)
 })
+
+const handleRefresh =  async(event: CustomEvent) => {
+    loaded.value = false
+    await getPickupHistory()
+    await getPickupOrder()
+    await getDelivery()
+    await getDeliveryHistory()
+    loaded.value = true
+    event.target?.complete();
+};
 
 const pickups = computed(() => {
     return store.getters['pickup/get']('pickupsList') as IPickupItem
@@ -130,7 +177,7 @@ const countDeliveryReq = computed(() => {
 
 const countDeliveryHist = computed(() => {
     return store.getters['arrive/get']('deliveryHist') as IDeliveryOrder || {}
-   
+
 })
 
 const cancel = () => modal.value.$el.dismiss();
